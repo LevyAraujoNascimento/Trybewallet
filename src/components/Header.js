e2da4c05e2268da96d6895bd15942b901cbc57d2
Levy Araujo <levy4raujo@gmail.com>
2023-02-24 13:07:51 -0300

    2° requisito concluído

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
+    const valor = 0;
+    const cambio = 'BRL';
     return (
-      <div>Header</div>
+      <div>
+        <h2
+          data-testid="email-field"
+        >
+          { email }
+        </h2>
+        <h2
+          data-testid="total-field"
+        >
+          { valor }
+        </h2>
+        <h2
+          data-testid="header-currency-field"
+        >
+          { cambio }
+        </h2>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
