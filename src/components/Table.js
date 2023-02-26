1f7cb7553f7ad981a0c59782ae47c3bed4cdbae7
Levy Araujo <levy4raujo@gmail.com>
2023-02-26 12:18:18 -0300

    7° requisito concluído

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import './compCSS.css';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    const despesas = expenses.map((element) => {
+      const { name } = element.exchangeRates[element.currency];
+      const { ask } = element.exchangeRates[element.currency];
+      const numValue = parseFloat(element.value);
+      const numAsk = parseFloat(ask);
+      const total = numAsk * numValue;
+      return (
+        <tr key={ element.id }>
+          <td>{ element.description }</td>
+          <td>{ element.tag }</td>
+          <td>{ element.method }</td>
+          <td>{ parseFloat(element.value).toFixed(2)}</td>
+          <td>{ name }</td>
+          <td>{ parseFloat(ask).toFixed(2) }</td>
+          <td>{ total.toFixed(2) }</td>
+          <td>Real</td>
+        </tr>
+      );
+    });
     return (
       <div>
-        <table>
-          <thead>
-            <tr>
+        <table id="table">
+          <thead id="table-header">
+            <tr id="table-list">
               <th>Descrição</th>
               <th>Tag</th>
               <th>Método de pagamento</th>
@@ -18,10 +41,21 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            { despesas }
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
