2a9f0467b1073e77a0a8b476ec43afa70862c7a6
Levy Araujo <levy4raujo@gmail.com>
2023-02-26 16:25:51 -0300

    Correção Lint 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { deleteExpense, removeMoney, editOn } from '../redux/actions';
-import './compCSS.css';
 
 class Table extends Component {
   deletar = (id, total) => {
