909905eef05bcead60461cecad1fb547a885c1c8
Levy Araujo <levy4raujo@gmail.com>
2023-02-27 08:53:23 -0300

    Correção Lint 5º requisito

--- src/tests/helpers/App.test.js
+++ src/tests/helpers/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { renderWithRouterAndRedux } from './renderWith';
 import { connect } from 'react-redux';
+import { renderWithRouterAndRedux } from './renderWith';
 import App from '../../App';
 import WalletForm from '../../components/WalletForm';
 
@@ -12,7 +12,7 @@ describe('Testando a Aplicação', () => {
   it('Deve renderizar a pagina de Login', () => {
     renderWithRouterAndRedux(<App />);
 
-    const inputs =  document.querySelectorAll('input');
+    const inputs = document.querySelectorAll('input');
     expect(inputs).toHaveLength(2);
 
     const botaoLogin = screen.queryByText('Entrar');
@@ -21,31 +21,34 @@ describe('Testando a Aplicação', () => {
 
   it('Testando o Router', () => {
     const { store } = renderWithRouterAndRedux(<App />);
-   
-    const inputs =  document.querySelectorAll('input');
+
+    const inputs = document.querySelectorAll('input');
     expect(inputs).toHaveLength(2);
 
     const email = inputs[0];
     const senha = inputs[1];
 
-    userEvent.type(email, 'Levy@gmail.com');
-    expect(email.value).toBe('Levy@gmail.com');
+    const emailTest = 'Levy@gmail.com';
+    const passwordTest = '123456';
 
-    userEvent.type(senha, '123456');
-    expect(senha.value).toBe('123456');
-   
+    userEvent.type(email, emailTest);
+    expect(email.value).toBe(emailTest);
+
+    userEvent.type(senha, passwordTest);
+    expect(senha.value).toBe(passwordTest);
+
     const botaoLogin = screen.queryByText('Entrar');
     expect(botaoLogin).toBeInTheDocument();
 
     userEvent.click(botaoLogin);
     expect(screen.getByText('Wallet Form')).toBeInTheDocument();
 
-    expect(store.getState().user.email).toBe('Levy@gmail.com');
+    expect(store.getState().user.email).toBe(emailTest);
   });
 
   it('Preenchendo os campos', async () => {
     renderWithRouterAndRedux(<WalletForm />);
-   
+
     const valor = screen.getByTestId('value-input');
     const descricao = screen.getByTestId('description-input');
     const moeda = screen.getByTestId('currency-input');
@@ -57,10 +60,10 @@ describe('Testando a Aplicação', () => {
     expect(moeda).toBeInTheDocument();
     expect(pagamento).toBeInTheDocument();
     expect(atividade).toBeInTheDocument();
-   
-    userEvent.type(valor,'10');
-    userEvent.type(descricao,'hotdog');
-   
+
+    userEvent.type(valor, '10');
+    userEvent.type(descricao, 'hotdog');
+
     const addDespesa = screen.getByText(/Adicionar despesa/i);
     expect(addDespesa).toBeInTheDocument();
 
@@ -75,5 +78,4 @@ describe('Testando a Aplicação', () => {
       expect(screen.getByText('USD').selected).toBe(true);
     });
   });
-
 });
